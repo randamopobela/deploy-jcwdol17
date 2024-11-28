a1334c27a4b4a13720e3c17a5dcae0aa490638b9
randamopobela <randamopobela@gmail.com>
2024-11-28 22:05:22 +0900

    feat: edit header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <h1 className="text-4xl font-bold">TEST DEPLOYMENT</h1>
+                <h1 className="text-4xl font-bold">
+                    TEST DEPLOYMENT by ANUGERAH
+                </h1>
                 <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
                     <li className="mb-2">
                         Get started by editing{" "}
